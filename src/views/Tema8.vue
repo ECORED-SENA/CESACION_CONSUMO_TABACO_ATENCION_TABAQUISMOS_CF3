8a9cd1c5b82dc00f039fbd43f14526d3cde184d0
Sergio Camacho <sergio.camachoo@misena.edu.co>
2021-09-22 10:14:39 -0500

    Ajustes de video

--- src/views/Tema8.vue
+++ src/views/Tema8.vue
@@ -10,7 +10,7 @@
 
     figure.mb-5(data-aos="fade-right" data-aos-duration="500")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/qUL5FLa-R74" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .titulo-sexto.color-acento-contenido(data-aos="fade-down" data-aos-offset="250" data-aos-duration="500")
       h5 Tabla 9
